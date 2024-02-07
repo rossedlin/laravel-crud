adbe2088ca3a5639ae378417e08ef865db930201
Ross Edlin <rossedlin@gmail.com>
2024-02-07 10:55:15 +0000

    Update deployment

---
+++ .github/environments/live/update.sh
@@ -0,0 +1,11 @@
+
+cd ~/github/laravel-crud || exit 1;
+
+git add .
+git reset HEAD --hard
+git fetch
+git pull
+
+/opt/alt/php82/usr/bin/php ~/composer.phar install --no-dev --prefer-dist --no-ansi --no-interaction --no-progress
+
+/opt/alt/php82/usr/bin/php artisan migrate:fresh --force
